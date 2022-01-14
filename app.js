27e5620dcff59b6255055bf6dcccbb441778d437
Dawid2803 <dawidjacoba@gmail.com>
2022-01-14 15:13:32 +0200

    added static filres route

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//enables app to serve static files
+app.use('/static', express.static('public'))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');

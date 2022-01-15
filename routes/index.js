9f4f71d390fe843d393a9742cac13d9ee8cdf577
Dawid2803 <dawidjacoba@gmail.com>
2022-01-15 17:04:42 +0200

    implemented update books feature

--- routes/index.js
+++ routes/index.js
@@ -40,13 +40,15 @@ router.post('/books/new', asyncHandler(async (req, res) => {
 
 //get book details
 router.get('/books/:id', asyncHandler(async (req, res) => {
-  const bookId = req.params.id;
-  res.send(bookId);
+  const book = await Book.findByPk(req.params.id);
+  res.render('update-book', { book, title: 'Update Book'} )
 }));
 
+//update book details
 router.post('/books/:id', asyncHandler(async (req, res) => {
-  const bookId = req.params.id;
-  res.send(bookId);
+  const book = await Book.findByPk(req.params.id);
+  await book.update(req.body);
+  res.redirect('/books');
 }));
 
 router.post('/books/:id/delete', asyncHandler(async (req, res) => {
